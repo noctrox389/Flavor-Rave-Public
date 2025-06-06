9311af219e45e6e71b329e3ef2315e65eb725bbf
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2025-06-06 16:59:14 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -2,4 +2,24 @@ package;
 
 #if !macro
 import Paths;
+//Mobile Controls
+ import mobile.flixel.FlxHitbox;
+ import mobile.flixel.FlxVirtualPad;
+ import mobile.flixel.input.FlxMobileInputID;
+ import mobile.backend.SUtil;
+ import mobile.backend.MobileScaleMode;
+ 
+ //Android
+ #if android
+ import android.content.Context as AndroidContext;
+ import android.widget.Toast as AndroidToast;
+ import android.os.Environment as AndroidEnvironment;
+ import android.Permissions as AndroidPermissions;
+ import android.Settings as AndroidSettings;
+ import android.Tools as AndroidTools;
+ import android.os.Build.VERSION as AndroidVersion;
+ import android.os.Build.VERSION_CODES as AndroidVersionCode;
+ import android.os.BatteryManager as AndroidBatteryManager;
+ #end
+import flixel.util.FlxDestroyUtil;
 #end
