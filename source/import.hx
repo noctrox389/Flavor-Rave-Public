29b296731d4564bea63134721bb5b8bf1d6229f6
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2025-06-07 18:43:16 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -19,7 +19,6 @@ import Paths;
  import extension.androidtools.Tools as AndroidTools;
  import extension.androidtools.os.Build.VERSION as AndroidVersion;
  import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
- import android.os.BatteryManager as AndroidBatteryManager;
  #end
  
 //Flixel
