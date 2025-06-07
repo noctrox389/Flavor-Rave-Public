afdf6d0d7df18c7d7ac09713c711da62092b11a9
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2025-06-07 12:37:25 +0800

    Update import.hx
    ima just add all the flixel imports here cos i dont wanna add each individual one one by one lol

--- source/import.hx
+++ source/import.hx
@@ -21,5 +21,22 @@ import Paths;
  import android.os.Build.VERSION_CODES as AndroidVersionCode;
  import android.os.BatteryManager as AndroidBatteryManager;
  #end
+ 
+//Flixel
+import flixel.sound.FlxSound;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.util.FlxDestroyUtil;
+
+using StringTools;
 #end
