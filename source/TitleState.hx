f86cd928d0500b5122ab538680691ae76eadb157
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2025-06-11 22:34:34 +0800

    Reverting
    Bro since when was there another error here??
    Ima temporarily patch it up first cos the voices problem is more prominent rn

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -46,7 +46,6 @@ import sys.io.File;
 
 class TitleState extends MusicBeatState
 {
-	private var video:VideoHandler;
 	public static var muteKeys:Array<FlxKey> = [FlxKey.ZERO];
 	public static var volumeDownKeys:Array<FlxKey> = [FlxKey.NUMPADMINUS, FlxKey.MINUS];
 	public static var volumeUpKeys:Array<FlxKey> = [FlxKey.NUMPADPLUS, FlxKey.PLUS];
@@ -295,7 +294,7 @@ class TitleState extends MusicBeatState
 		if (!initialized)
 		{
 			#if VIDEOS_ALLOWED
-			video = new VideoHandler();
+			var video:VideoHandler = new VideoHandler();
 			video.canSkip = true;
 			video.skipKeys = [FlxKey.ESCAPE, FlxKey.ENTER];
 			video.onEndReached.add(function()
@@ -359,21 +358,6 @@ class TitleState extends MusicBeatState
 
 		if (FlxG.sound.music != null)
 			Conductor.songPosition = FlxG.sound.music.time;
-		#if mobile
-	        if (video != null && video.canSkip)
-	        {
-	            for (touch in FlxG.touches.list)
-	            {
-	                if (touch.justPressed)
-	                {
-	                    video.canSkip = false;
-	                    video.onEndReached.dispatch();
-	                    video = null;
-	                    return; 
-	                }
-	            }
-	        }
-		#end
 
 		var pressedEnter:Bool = canInput && (FlxG.keys.justPressed.ENTER || controls.ACCEPT || FlxG.mouse.justPressed && ClientPrefs.menuMouse);
 
@@ -506,9 +490,6 @@ class TitleState extends MusicBeatState
 
 	function skipIntro():Void
 	{
-		if (skippedIntro) return;
-    		skippedIntro = true;
-		
 		if (!initialized)
 		{
 			if (firstStart) FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
