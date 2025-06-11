553f69b5acc5804f5d22873dbf61e4d471dbfeb1
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2025-06-11 15:15:08 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -7,7 +7,6 @@ import Paths;
  import mobile.flixel.FlxVirtualPad;
  import mobile.flixel.input.FlxMobileInputID;
  import mobile.backend.SUtil;
- import mobile.backend.MobileScaleMode;
  
  //Android
  #if android
